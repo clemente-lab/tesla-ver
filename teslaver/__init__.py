4a6d9c69268a91747d5289069bac4bc46450cbe2
Jose Carlos Clemente Litran <jose.clemente@gmail.com>
2018-10-19 10:40:07 -0400

    Initial commit

---
+++ teslaver/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+__author__ = "The Clemente Lab"
+__copyright__ = "Copyright 2018, The Clemente Lab"
+__credits__ = ["Jose C. Clemente"]
+__license__ = "GPL"
+__version__ = "0.1.0-dev"
+__maintainer__ = "Jose C. Clemente"
+__email__ = "jose.clemente@gmail.com"
+
+__all__ = [
+           ]
