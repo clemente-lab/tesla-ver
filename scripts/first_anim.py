82415f8f4f653025262ab39794f9365b7f69edcf
Alexander Kyim <alexanderkyim@gmail.com>
2018-10-24 14:02:50 -0400

    Added comments, and fixed spacing

--- scripts/first_anim.py
+++ scripts/first_anim.py
@@ -1,4 +1,6 @@
+#imports the main plotly library as plty
 import plotly.plotly as plty
+#imports the Grid and Column objects from grid_objects
 from plotly.grid_objs import Grid, Column
 
 import time
@@ -11,11 +13,14 @@ column_4 = Column([1.5], 'y2')
 column_5 = Column([2.0], 'x3')
 column_6 = Column([3.0], 'y3')
 
+#Creates a variable called grid, which is a new Grid comprised of the 6 column variables
 grid = Grid([column_1, column_2, column_3, column_4, column_5, column_6])
+#Creates a new graph grid on the server, named tut_anim, without opening it and comprised of the Grid grid
 plty.grid_ops.upload(grid, 'tut_anim'+str(time.time()), auto_open = False)
 
 #Creates the animation
 figure = {
+    #Defines the starting point for the animation
     'data': [
         {
             'xsrc': grid.get_column_reference('x3'),
@@ -23,21 +28,27 @@ figure = {
             'mode': 'markers',
         }
     ],
-    'layout': {'title': 'Ping Pong Animation',
-               'xaxis': {'range': [0, 2], 'autorange': False},
-               'yaxis': {'range': [0, 2], 'autorange': False},
-               'updatemenus': [{
+    #Defines the layout, where the initial range shown, the title, and any buttons are defined.
+    'layout': {
+                'title': 'Ping Pong Animation',
+                'xaxis': {'range': [0, 2], 'autorange': False},
+                'yaxis': {'range': [0, 2], 'autorange': False},
+                'updatemenus': [{
                    'buttons': [
-                       {'args': [None],
-                        'label': 'Play',
-                        'method': 'animate'}
-               ],
-               'pad': {'r': 10, 't': 87},
-               'showactive': False,
-               'type': 'buttons'
+                       {
+                           'args': [None],
+                            'label': 'Play',
+                            'method': 'animate'
+                    }
+                ],
+                'pad': {'r': 10, 't': 87},
+                'showactive': False,
+                'type': 'buttons'
                 }]},
+    #Creates the different frames of the animation
     'frames': [
         {
+            #Animation Frame 1, indicates the position to move the point to
             'data': [
                 {
                     'xsrc': grid.get_column_reference('x'),
@@ -46,17 +57,20 @@ figure = {
                 }
             ]
         },
+        #Animation Frame 2, see Animation Frame 1 comment for explanation
         {
             'data': [
                 {
                     'xsrc': grid.get_column_reference('x2'),
                     'ysrc': grid.get_column_reference('y2'),
                     'mode': 'markers',
+                    #Sets the color and size of the point
                     'marker': {'color': '#5405e5', 'size': 25}
                 }
             ]
         },
         {
+            #Animation Frame 3
             'data': [
                 {
                     'xsrc': grid.get_column_reference('x3'),
@@ -66,6 +80,7 @@ figure = {
                 }
             ]
         },
+        #Animation Frame 4
         {
             'data': [
                 {
@@ -78,4 +93,5 @@ figure = {
     ]
 }
 
+#Creates the animation on the server, with the title 'tut_anim' + the current time, using the animation frames defined in 'figure'
 plty.icreate_animations(figure, 'tut_anim'+str(time.time()))
--- scripts/first_graph.py
+++ scripts/first_graph.py
@@ -1,6 +1,8 @@
+#Imports the main plotly library and the graphing library
 import plotly.plotly as py
 import plotly.graph_objs as go
 
+#Creates a scatterplot from the selected values
 trace0 = go.Scatter(
     x=[1, 2, 3, 4],
     y=[10, 15, 13, 17]
@@ -9,6 +11,9 @@ trace1 = go.Scatter(
     x=[1, 2, 3, 4],
     y=[16, 5, 11, 9]
 )
+
+#Creates a list, data, made of the two scatterplots trace0 and trace1
 data = [trace0, trace1]
 
+#Creates a graph using the data variable, called 'basic-line', on the server (username and api key connected during setup)
 py.iplot(data, filename = 'basic-line')
