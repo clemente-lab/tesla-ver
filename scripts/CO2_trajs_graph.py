f0be5dcf48ef82233d57adec5c6ae959ed653711
Alexander Kyim <alexanderkyim@gmail.com>
2018-11-09 08:35:51 -0500

    Converted the graphing to use lodi trajectory objects

--- scripts/CO2_trajs_graph.py
+++ scripts/CO2_trajs_graph.py
@@ -1,29 +1,26 @@
-#Imports plotly  and plotly graphing libraries
 import plotly.plotly as py
 import plotly.graph_objs as go
-#Imports time module
 import time
-#Imports lodi and lodi parsing modules
 import lodi
 from lodi import parse
 
 #imports the dataset and parses it with the lodi parse method
-parsed_data = lodi.parse.read_csv('../teslaver/data/CO2_trajs.csv')
+parsed_data = lodi.parse.read_trajectories('../teslaver/data/CO2_trajs.csv')
 
 
-
+print parsed_data[0].time
 
 #Creats list to store all traced scatterplots
 data = list()
 
 #Iterates through the entire dataset and creates traces out of the data
-for counter in list(range(len(parsed_data['ID']))):
+for counter in list(range(len(parsed_data))):
     #Creates each traced scatterplot
     trace = go.Scattergl(
-        x = [int(val) for val in parsed_data['X'][counter].split(',')],
-        y = [float(val) for val in parsed_data['Y'][counter].split(',')],
+        x = parsed_data[counter].time,
+        y = parsed_data[counter].values,
         mode = 'lines+markers',
-        name= parsed_data['country_name'][counter]
+        name= parsed_data[counter].get_mdata()['country_name']
      )
      #Appends each traced scatterplot to data list
     data.append(trace)
@@ -52,42 +49,4 @@ layout= go.Layout(
 fig = go.Figure(data=data, layout=layout)
 
 #Plots the data on plotly
-py.plot(fig, filename = 'C02_Graph '+str(time.time()))
-
-# #How does one select the C02_trajs.csv file from the data directory?
-# with open('./data/CO2_trajs.csv', 'r') as main_csv:
-#     main_list = list(csv.reader(main_csv))
-#     #separate all of the list entries into their own sections (id and first year no longer in same entry)
-#     for counter in list(range(len(main_list))):
-#             main_list[counter] = "\t".join(main_list[counter]).split()
-#             temp_year_list = list()
-#             temp_kg_list = list()
-#             val_indicies_to_remove = list()
-#             #Clump the x values (years) into their own sublist and the y values as well, across all lists
-#             for val in main_list[counter]:
-#                 # puts x values into their own list
-#                 if len(val) == 4 and (val.startswith('20') or val.startswith('19')):
-#                     temp_year_list.append(val)
-#                     val_indicies_to_remove.append(val)
-#                 #puts y values into their own list
-#                 if val.startswith('0.'):
-#                     temp_kg_list.append(val)
-#                     val_indicies_to_remove.append(val)
-#             for val in val_indicies_to_remove:
-#                 main_list[counter].remove(val)
-#             #Replaces the new list versions of the year values into the lists at the appropriate indicies
-#             main_list[counter].insert(1, temp_year_list)
-#             main_list[counter].insert(2, temp_kg_list)
-# data = list()
-# #Adds each plot to trace to a list
-# for counter in list(range(len(main_list))):
-#     trace = go.Scattergl(
-#         x = main_list[counter][1],
-#         y = main_list[counter][2],
-#         mode = 'lines+markers',
-#         #Imperfectly adds the labels to each country's graph
-#         text=main_list[counter][len(main_list[counter])-21]
-
-#     )
-#     data.append(trace)
-#Plots data
+py.plot(fig, filename = 'C02_Graph'+str(time.time()))
