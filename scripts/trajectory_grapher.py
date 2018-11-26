063bb3c4cc820583ef01d60f71ed6a0f8f7699d0
Alexander Kyim <alexanderkyim@gmail.com>
2018-11-26 11:47:42 -0500

    Fixed mdata and value cases

--- scripts/trajectory_grapher.py
+++ scripts/trajectory_grapher.py
@@ -12,7 +12,7 @@ def graph_trajectories(path, graph_title, xaxis_title, yaxis_title, trajectory_n
         graph_title: the title for top of the graph
         xaxis_title: the x-axis title
         yaxis_title: the y-axis title
-
+        trajectory_name: the name for each plotted line/trajectory
     """
 
     #imports the dataset and parses it with the lodi parse method
@@ -24,55 +24,36 @@ def graph_trajectories(path, graph_title, xaxis_title, yaxis_title, trajectory_n
     #Iterates through the entire dataset and creates traces out of the data
     for traj in trajectories:
         
-        if xaxis_title in traj.get_mdata() and yaxis_title in traj.get_mdata():
-            #checks lists for equality of length
-            if len(traj.get_mdata()[xaxis_title]) == len(traj.get_mdata()[yaxis_title]):
-                #Creates each traced scatterplot
-                trace = go.Scattergl(
-                    x = list(map(float, traj.get_mdata()[xaxis_title])),
-                    y = list(map(float, traj.get_mdata()[yaxis_title])),
-                    mode = 'lines+markers',
-                    name= traj.get_mdata()[trajectory_name]
-                )
-                #Appends each traced scatterplot to data list
-                data.append(trace)
-        elif xaxis_title == 'times' and yaxis_title == 'values':
-            #Creates each traced scatterplot
-                trace = go.Scattergl(
-                    x = traj.time,
-                    y = traj.values,
-                    mode = 'lines+markers',
-                    name= traj.get_mdata()[trajectory_name]
-                )
-                #Appends each traced scatterplot to data list
-                data.append(trace)
-        elif xaxis_title == 'times' and yaxis_title in traj.get_mdata():
-            #Creates each traced scatterplot
-                trace = go.Scattergl(
-                    x = traj.time,
-                    y = list(map(float, traj.get_mdata()[yaxis_title])),
-                    mode = 'lines+markers',
-                    name= traj.get_mdata()[trajectory_name]
-                )
-                #Appends each traced scatterplot to data list
-                data.append(trace)
-        elif xaxis_title in traj.get_mdata() and yaxis_title == 'values':
-                #Creates each traced scatterplot
-                trace = go.Scattergl(
-                    x = list(map(float, traj.get_mdata()[xaxis_title])),
-                    y = traj.values,
-                    mode = 'lines+markers',
-                    name= traj.get_mdata()[trajectory_name]
-                )
-                #Appends each traced scatterplot to data list
-                data.append(trace)         
+        #Checks whether or not xaxis_title is mdata, and assigns appropriate data to the lists
+        if xaxis_title == 'time':
+            x_values = traj.time
+        elif xaxis_title in traj.get_mdata():
+            x_values = list(map(float, traj.get_mdata()[xaxis_title]))
         else:
-            print 'Invalid Parameters passed'
-            exit()
+            print 'Invalid Xaxis input'
+            exit
+        
+        #Checks whether or not yaxis_title is mdata, and assigns appropriate data to the lists
+        if yaxis_title == 'values':
+            y_values = traj.values
+        elif yaxis_title in traj.get_mdata():
+            y_values = list(map(float, traj.get_mdata()[yaxis_title]))
+        else:
+            print 'Invalid Yaxis input'
+            exit
+        
+        trace = go.Scattergl(
+                    x = x_values,
+                    y = y_values,
+                    mode = 'lines+markers',
+                    name= traj.get_mdata()[trajectory_name]
+                )
+        #Appends each traced scatterplot to data list
+        data.append(trace)
 
     #Creates layout of the graph
     layout= go.Layout(
-        title= graph_title,
+        title= graph_title + ' ' + str(time.time()),
         hovermode= 'closest',
         xaxis= dict(
             title= xaxis_title,
