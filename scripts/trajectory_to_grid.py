1d61a242755030d0258f68a734218a7731dae3d6
Alexander Kyim <alexanderkyim@gmail.com>
2018-12-11 08:57:06 -0500

    Added Dataframe builder for normal values

--- scripts/trajectory_to_grid.py
+++ scripts/trajectory_to_grid.py
@@ -14,41 +14,44 @@ def trajectory_to_grid(path, yaxis):
         #makes list of all potential time values  
         xvalues = traj.time
         #TODO: Sort them into order
-    columns_list.append('xavlues')
-    values_list = np.empty(shape = (len(xvalues), len(trajectory)))
+    # columns_list.append('xavlues')
+    # values_list = np.empty(shape = (len(xvalues), len(trajectory)))
+    returnFrame = pd.DataFrame({'xvalues': xvalues})
     for num, traj in enumerate(trajectory):
         #Checks whether or not yaxis is mdata, and assigns appropriate data to the lists
         if yaxis == 'values':
             #supposed to return a list where if a year doesnt overlap, contains a true values, and otherwise has a false value,
             #from there, theres a loop to basically iterate through that trajectory, and puts the value for a class
-            none_list = np.isin(traj.time, xvalues, True, True)
-            #
-            print none_list
-            yvalues = list(traj.values)
-            yvalues_populated = list()
-            for val in none_list:
-                if val:
-                    yvalues_populated.append(val)
-                else:
-                    yvalues_populated.append(None)
-            print yvalues_populated
-            values_list[:, num] = yvalues_populated
+            yvalues = traj.values
+            while(len(yvalues) < len(xvalues)):
+                yvalues = np.insert(yvalues, 0, None)
+            # yvalues = np.vstack(yvalues)
             columns_list.append('yvalues' + str(num))
-        elif yaxis in traj.get_mdata():
-            yvalues = list(map(float, traj.get_mdata()[yaxis]))
-            none_list = np.in1d(traj.time, xvalues, True, True)
-            yvalues_populated = list()
-            for val in none_list:
-                if val in yvalues:
-                    yvalues_populated.append(val)
-                else:
-                    yvalues.append(None)
-            values_list.append(yvalues_populated)
-            columns_list.append('yvalues' + str(num))
+            data_main = {columns_list[num]: yvalues}
+            df_joined = pd.DataFrame(data=data_main)
+            returnFrame.join(df_joined)
+            # yvalues = list(traj.values)
+            # yvalues_populated = list()
+            # for val in none_list:
+            #     if val:
+            #         yvalues_populated.append(val)
+            # values_list[:, num] = yvalues_populated
+            # columns_list.append('yvalues' + str(num))
+        # elif yaxis in traj.get_mdata():
+        #     yvalues = list(map(float, traj.get_mdata()[yaxis]))
+        #     none_list = np.in1d(traj.time, xvalues, True, True)
+        #     yvalues_populated = list()
+        #     for val in none_list:
+        #         if val in yvalues:
+        #             yvalues_populated.append(val)
+        #         else:
+        #             yvalues_populated.append(None)
+        #     values_list.append(yvalues_populated)
+        #     columns_list.append('yvalues' + str(num))
         else:
             print 'Invalid Yaxis input'
             exit
-    return values_list
+    return True
     #TODO: Create code to insert None values in between all values which arent set up correctly
     # dframe_return = pd.DataFrame(values_list, columns=columns_list)
     # columns_list2 = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th']
@@ -57,4 +60,4 @@ def trajectory_to_grid(path, yaxis):
     # combined = np.vstack((np1, np2)).T
     # return pd.DataFrame(combined, columns = ['1st', '2nd'])
     
-print trajectory_to_grid('../teslaver/data/CO2_trajs.csv', 'values')
+trajectory_to_grid('../teslaver/data/CO2_trajs.csv', 'values')
