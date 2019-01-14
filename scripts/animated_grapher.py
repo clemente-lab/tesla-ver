67115492a955b283738ef87e61bba9fbea1a1245
Alexander Kyim <alexanderkyim@gmail.com>
2019-01-13 21:07:05 -0500

    updated layout

--- scripts/animated_grapher.py
+++ scripts/animated_grapher.py
@@ -21,7 +21,51 @@ def animate_trajectories(path, graph_title, xaxis_title, yaxis_title, trajectory
 
     figure['layout']['xaxis'] = {'range': [min(dtfr[0]), max(dtfr[0])], 'title': xaxis_title, 'gridcolor': '#FFFFFF'}
     figure['layout']['yaxis'] = {'title': yaxis_title, 'range': [np.nanmin(dtfr), np.nanmax(dtfr[:, 1:])]}
+    }
+    sliders_dict = {
+        'active': 0,
+        'yanchor': 'top',
+        'xanchor': 'left',
+        'currentvalue': {
+            'font': {'size': 20},
+            'prefix': 'Year:',
+            'visible': True,
+            'xanchor': 'right'
+        },
+        'transition': {'duration': 300, 'easing': 'cubic-in-out'},
+        'pad': {'b': 10, 't': 50},
+        'len': 0.9,
+        'x': 0.1,
+        'y': 0,
+        'steps': []
+    }
 
+    figure['layout']['updatemenus'] = [
+        {
+            'buttons': [
+                {
+                    'args': [None, {'frame': {'duration': 500, 'redraw': False},
+                                    'fromcurrent': True, 'transition': {'duration': 300, 'easing': 'quadratic-in-out'}}],
+                    'label': 'Play',
+                    'method': 'animate'
+                },
+                {
+                    'args': [[None], {'frame': {'duration': 0, 'redraw': False}, 'mode': 'immediate',
+                                      'transition': {'duration': 0}}],
+                    'label': 'Pause',
+                    'method': 'animate'
+                }
+            ],
+            'direction': 'left',
+            'pad': {'r': 10, 't': 87},
+            'showactive': False,
+            'type': 'buttons',
+            'x': 0.1,
+            'xanchor': 'right',
+            'y': 0,
+            'yanchor': 'top'
+        }
+    ]
 
     for counter in list(range(len(main_data.__len__))):
         data_dict = {
