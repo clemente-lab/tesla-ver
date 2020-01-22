4002eb2fbaaab7bca2810dd1061b8fddc9bad0bf
Alexander Kyim <39439486+alexanderkyim@users.noreply.github.com>
2020-01-22 10:26:50 -0500

    Update Docstring Formatting in __init__.py
    Based on suggestions from pull request #33
    
    Co-Authored-By: David Wallach <d.s.t.wallach@gmail.com>

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
 def create_app():
-  """Construct the Core application"""
+  """ Construct the Core application """
   app = Flask(__name__,
               instance_relative_config = False)
   app.config.from_object('config.Config')
 
   with app.app_context():
-    #blueprints
+    # Blueprints
     from application import routes
     app.register_blueprint(routes.main_bp)
 
