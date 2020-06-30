c17b7f529166b2a2de075bc6e62e0e95cc263387
Alexander Kyim <alexanderkyim@gmail.com>
2020-06-30 17:27:33 -0400

    updated setup.py and __init__.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import ast
 import re
 
 __author__ = "The Clemente Lab"
-__copyright__ = "Copyright (c) 2018 The Clemente Lab"
+__copyright__ = "Copyright (c) 2020 The Clemente Lab"
 __credits__ = ["Alexander Kyimpopkin", "David Wallach", "Jose C. Clemente"]
 __license__ = "GPL"
 __maintainer__ = "Alexander Kyimpopkin"
