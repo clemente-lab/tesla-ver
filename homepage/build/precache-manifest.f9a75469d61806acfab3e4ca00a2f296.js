4d6868b765418b93a1b9b4a838b166cff52c737c
Alexander Kyim <alexanderkyim@gmail.com>
2020-07-28 16:45:05 -0400

    added build folder back

---
+++ homepage/build/precache-manifest.f9a75469d61806acfab3e4ca00a2f296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8661044b05f14b53cfd9f608f2840384",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e86889fff98aeee9739",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "57815eeb2680e7b0164d",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e86889fff98aeee9739",
+    "url": "./static/js/2.3d8fc1b0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.3d8fc1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57815eeb2680e7b0164d",
+    "url": "./static/js/main.376af520.chunk.js"
+  },
+  {
+    "revision": "c27f27df623d972913a8",
+    "url": "./static/js/runtime-main.fb7466c0.js"
+  }
+]);
