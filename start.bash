12dc78381d676f4d4ff8f565466924527c4e654a
Alexander Kyim <alexanderkyim@gmail.com>
2020-08-18 12:14:34 -0400

    install and startup script updated

--- start.bash
+++ start.bash
@@ -50,7 +50,21 @@ trap "shutdown_with_redis" SIGINT
 
 if ! conda_loc="$(type -p "conda")";
 then
-  echo "please install conda :)"
+  echo "Conda installation not found"
+  echo "By installing, you agree to the conda license at https://docs.conda.io/en/latest/license.html"
+  read -r -p "Install Conda for Linux? [y/N] " response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+  then
+      read -p "Mac or Linux (M/L)?" choice
+      case "$choice" in
+        m|M ) os_name="MacOSX";;
+        l|L ) os_name="Linux";;
+        * ) echo "invalid selection" && exit 1;;
+      esac
+      curl -so - "https://repo.anaconda.com/miniconda/Miniconda3-latest-$os_name-x86_64.sh" | bash - -b -p $HOME/miniconda
+  else
+      exit 1
+  fi
 else
   CONDA_PREFIX=$(conda info | grep -i "base environment" | grep -oEe "\w{3,4}conda3")
   source ~/$CONDA_PREFIX/etc/profile.d/conda.sh
@@ -66,5 +80,5 @@ else
   fi
 fi
 
-gunicorn --workers=2 --chdir=/tesla-ver/ --bind=0.0.0.0:5000 wsgi:server
+gunicorn --workers=2 --bind=0.0.0.0:5000 wsgi:server
 
