7691ff8dc378917e54547ab9a6bc178eff2e71ca
Alexander Kyim <alexanderkyim@gmail.com>
2020-08-27 14:06:58 -0400

    added gunicorn logging support and handling

--- wsgi.py
+++ wsgi.py
@@ -6,54 +6,51 @@ from tesla_ver.bubble_chart.bubble_chart import generate_bubble_chart
 from tesla_ver.data_uploading.data_uploading import generate_data_uploading
 
 
-# Sets up logging
-
-logging.basicConfig(
-    filename=str(datetime.now()) + ".log",
-    filemode="w",
-    format="'%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.DEBUG,
-)
-logging.debug(f"==Log Start==")
-# Creates the flask server
-logging.debug("Starting Flask Server")
-
 server = flask.Flask(__name__, static_folder="homepage/build/static", template_folder="homepage/build/")
 
-logging.debug("✅ Flask server created")
+# If application is being run through gunicorn, pass logging through to gunicorn
+if __name__ != "__main__":
+    # Connects flask logging to the gunicorn logging handler
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    server.logger.handlers = gunicorn_logger.handlers
+    server.logger.setLevel(gunicorn_logger.level)
+
+server.logger.debug("✅ Flask server created")
 
 server.wsgi_app = DebuggedApplication(server.wsgi_app, evalex=True)
 
-logging.debug("Werkzeug DebuggedApplication Created")
+server.logger.debug("Werkzeug DebuggedApplication Created")
 # Creates the data uploading screena and connects it to the flask server
 generate_data_uploading(server=server)
-logging.debug("✅ Data Uploading Screen created and connected")
+server.logger.debug("✅ Data Uploading Screen created and connected")
 
 # Creates the bubble chart and connects it to the flask server
 generate_bubble_chart(server=server)
 
-logging.debug("✅ Bubble Chart Screen created and connected")
+server.logger.debug("✅ Bubble Chart Screen created and connected")
 
 
 @server.route("/")
 def index():
     """Renders the landing page."""
-    # TODO: Create an HTML import function
+    server.logger.debug("rendering homepage")
     return flask.render_template("index.html")
 
 
 @server.route("/bubblechart.html")
 def render_bubble_chart():
     """Redirects to the Dash Bubble chart."""
+    server.logger.debug("redirecting to bubblechart")
     return flask.redirect("/bubblechart.html")
 
 
 @server.route("/datauploading.html")
 def render_data_uploading():
+    server.logger.debug("redirecting to data uploader")
     return flask.redirect("/datauploading.html")
 
 
 if __name__ == "__main__":
-    logging.debug("Server starting")
+    server.logger.debug("Server starting")
     server.run(debug=True)
-    logging.debug("✅ Server started")
+    server.logger.debug("✅ Server started")
