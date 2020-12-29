d3c720c63465b7e940c60cb1fe09e61dd4f2ae11
Alexander Kyim <alexanderkyim@gmail.com>
2020-12-28 22:09:42 -0500

    added homepage item testing

--- tests/homepage/test_page_contents.py
+++ tests/homepage/test_page_contents.py
@@ -1,6 +1,29 @@
 import pytest
+from playwright import _page
 
 
 def test_title(page):
-    page.goto("http://localhost:5000")
+    """Checks for correct page title
+
+    Args:
+        page : pytest-playwright fixture object
+    """
+    page.goto("/")
     assert page.title() == "Tesla-ver"
+
+
+def test_chart_button(page):
+    page.goto("/")
+    # page.evalOnSelector("")
+    assert page.innerText("css=div .btn.btn-success") == "Bubble Chart"
+
+
+def test_data_upload_button(page):
+    page.goto("/")
+    # page.evalOnSelector("")
+    assert page.innerText("css=div .btn.btn-primary") == "Data Uploading"
+
+
+def test_paragraph_header(page):
+    page.goto("/")
+    assert len(page.querySelectorAll("p")) == 2
