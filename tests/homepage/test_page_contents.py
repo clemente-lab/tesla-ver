e310df05408b4a8ed1f41389c67a5917dd3edbde
Alexander Kyim <alexanderkyim@gmail.com>
2021-01-19 08:49:31 -0500

    added docstring to test_chart_button

--- tests/homepage/test_page_contents.py
+++ tests/homepage/test_page_contents.py
@@ -6,21 +6,24 @@ def test_title(page):
     """Checks for correct page title
 
     Args:
-        page : pytest-playwright fixture object
+        page : playwright browser page
     """
     page.goto("/")
     assert page.title() == "Tesla-ver"
 
 
 def test_chart_button(page):
+    """Tests that chart button is styled correctly and has the right name
+
+    Args:
+        page : playwright browser page
+    """
     page.goto("/")
-    # page.evalOnSelector("")
     assert page.innerText("css=div .btn.btn-success") == "Bubble Chart"
 
 
 def test_data_upload_button(page):
     page.goto("/")
-    # page.evalOnSelector("")
     assert page.innerText("css=div .btn.btn-primary") == "Data Uploading"
 
 
