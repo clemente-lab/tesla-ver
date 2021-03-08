054c0eb4a518ac0517f0582dce47bdb46ca586a3
Alexander Kyim <alexanderkyim@gmail.com>
2021-03-08 13:33:53 -0500

    updated testing

--- tests/homepage/test_page_contents.py
+++ tests/homepage/test_page_contents.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def test_title(page):
     """Checks for correct page title
 
@@ -9,23 +6,3 @@ def test_title(page):
     """
     page.goto("/")
     assert page.title() == "Tesla-ver"
-
-
-def test_chart_button(page):
-    """Tests that chart button is styled correctly and has the right name
-
-    Args:
-        page : playwright browser page
-    """
-    page.goto("/")
-    assert page.innerText("css=div .btn.btn-success") == "Bubble Chart"
-
-
-def test_data_upload_button(page):
-    page.goto("/")
-    assert page.innerText("css=div .btn.btn-primary") == "Data Uploading"
-
-
-def test_paragraph_header(page):
-    page.goto("/")
-    assert len(page.querySelectorAll("p")) == 2
