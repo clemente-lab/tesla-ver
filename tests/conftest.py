52639c9b3dd1a273bb9089a05aa30024047f1a20
Alexander Kyim <alexanderkyim@gmail.com>
2021-03-07 23:16:42 -0500

    updated pytest configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,13 @@
 import os
 
-pytest_plugins = ("playwright", "parallel")
+pytest_plugins = ("playwright")
 
 import pytest
 
 
 @pytest.fixture(scope="session")
 def browser_context_args(browser_context_args):
-    return {**browser_context_args, "ignoreHTTPSErrors": True}
-
-
-def pytest_addoption(parser):
-    parser.addoption("--url", action="store", default="http://localhost:5000", help="target url")
-
-
-def pytest_configure(config):
-    os.environ["url"] = config.getoption("url")
-
+    return {
+        **browser_context_args,
+        "ignore_https_errors": True
+    }
